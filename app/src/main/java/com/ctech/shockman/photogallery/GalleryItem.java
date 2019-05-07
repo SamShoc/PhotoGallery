25aadeaa2ea86b18e6ec5b226dcf7a85d1e95aaa
SamShoc <7794501@isd535.org>
2019-05-06 20:08:39 -0500

    got to page 19 but code is still broken

---
+++ app/src/main/java/com/ctech/shockman/photogallery/GalleryItem.java
@@ -0,0 +1,37 @@
+package com.ctech.shockman.photogallery;
+
+public class GalleryItem {
+
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+
+    public String getmId() {
+        return mId;
+    }
+
+    public void setmId(String mId) {
+        this.mId = mId;
+    }
+
+    public String getmCaption() {
+        return mCaption;
+    }
+
+    public void setmCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+}
--- app/src/main/java/com/ctech/shockman/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/ctech/shockman/photogallery/PhotoGalleryFragment.java
@@ -2,6 +2,7 @@ package com.ctech.shockman.photogallery;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.service.autofill.TextValueSanitizer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,12 +12,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
 public class PhotoGalleryFragment extends Fragment {
+
+    private static final String TAG = "PhotoGalleryFragment";
+
+    private List<GalleryItem> mItems = new ArrayList<>();
 
     private RecyclerView mPhotoRecyclerView;
 
@@ -41,7 +49,54 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView = v.findViewById(R.id.photo_recycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
+        setupAdapter();
+
         return v;
+    }
+
+    private void setupAdapter() {
+        if (isAdded()) {
+            mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
+        }
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+
+            mTitleTextView = (TextView) itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem item) {
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+        private List<GalleryItem> mGalleryItems;
+
+        public PhotoAdapter(List<GalleryItem> galleryItems) {
+            mGalleryItems = galleryItems;
+        }
+
+        @Override
+        public PhotoHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            TextView textView = new TextView(getActivity());
+            return new PhotoHolder(textView);
+        }
+
+        @Override
+        public void onBindVViewHolder(PhotoHolder photoHolder, int position) {
+            GalleryItem galleryItem = mGalleryItems.get(position);
+            photoHolder.bindGalleryItem(galleryItem);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGalleryItems.size();
+        }
     }
 
     private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
