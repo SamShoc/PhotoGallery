5a635c61f9772a465940c30a9c1f93d64bb3d91e
SamShoc <7794501@isd535.org>
2019-05-09 08:04:12 -0500

    finished the unit and the app displayes pictures from flickr

--- app/src/main/java/com/ctech/shockman/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/ctech/shockman/photogallery/PhotoGalleryFragment.java
@@ -1,8 +1,11 @@
 package com.ctech.shockman.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.service.autofill.TextValueSanitizer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -38,7 +41,18 @@ public class PhotoGalleryFragment extends Fragment {
         setRetainInstance(true);
         new FetchItemsTask().execute();
 
-        mThumbnailDownloader = new ThumbnailDownloader<>();
+        Handler responseHandler = new Handler();
+        mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
+        mThumbnailDownloader.setThumbnailDownloadListener(
+                new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>() {
+                    @Override
+                    public void onThumbnailDownloaded(PhotoHolder photoHolder, Bitmap bitmap) {
+                        Drawable drawable = new BitmapDrawable(getResources(), bitmap);
+                        photoHolder.bindDrawable(drawable);
+                    }
+                }
+        );
+
         mThumbnailDownloader.start();
         mThumbnailDownloader.getLooper();
         Log.i(TAG, "Background thread started.");
@@ -63,6 +77,12 @@ public class PhotoGalleryFragment extends Fragment {
         if (isAdded()) {
             mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
         }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailDownloader.clearQueue();
     }
 
     @Override
